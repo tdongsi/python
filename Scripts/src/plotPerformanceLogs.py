d2ea8764a65b30aba90cd3166a493a05c5018c5f
Tue-Cuong Dong-Si <dongsi.tuecuong@gmail.com>
2014-04-01 17:29:30 -0700

    Initial script to plot performance metrics against baseline numbers.

---
+++ Scripts/src/plotPerformanceLogs.py
@@ -0,0 +1,168 @@
+#!/usr/buildtools/bin/python2.7
+
+'''
+Created on Apr 1, 2014
+
+@author: cuongd
+
+USAGE: scriptName plotName.png {list of log file paths}
+NOTE: the plot file name must have png extension.
+
+This script will plot data parsed from log files into a png file.
+The png file name is specified by the first argument.
+The data files can be supplied as a list from the following arguments.
+NOTE: It assumes certain knowledge of structure and format of the log file.
+
+It will parse for scan time, disk reads, and buffer reads for each test with different number of threads.
+Parsed data of each metric, e.g. scan time, of all log files will be plotted into a subplot.
+The legends will be based on the file name (last 45 chars of filename), so pick a meaningful log file name.
+
+The Python script used Matplotlib library.
+Please install the following Python libraries: matplotlib, numpy, dateutil, pytz, pyparsing, six.
+(optionally pillow, pycairo, tornado, wxpython, pyside, pyqt, ghostscript, miktex, ffmpeg, mencoder, avconv, or imagemagick)
+
+I believe installation of these Python libraries are straight-forward on Linux and Win32.
+On Win64, please find installers here: http://www.lfd.uci.edu/~gohlke/pythonlibs/
+'''
+import matplotlib.pyplot as plt
+import numpy as np
+
+# # global setting for font size
+# font = {'family' : 'normal',
+#         'weight' : 'normal',
+#         'size'   : 6}
+# 
+# plt.rc('font', **font)
+
+
+'''
+parse data for all trials with given thread number
+Input:
+filename: the log file
+threadNum: the given thread number
+Output:
+1 x 3 array: average time, disk reads, buffer reads
+'''
+def parseDataThread(filename, threadNum):
+
+    f = open(filename)
+    lines = f.readlines()
+    
+    searchThreadPhrase = 'Thread: '
+    searchThreadPhrase += str(threadNum)
+    
+    endingMemoryPhrase = 'CPU usage:'
+    
+    # Flag of starting a test: "Thread: ? Attempt: ?" entry in log.
+    startFlag = False
+    time = []
+    diskReads = []
+    bufferReads = []
+    
+    for line in lines:
+        # Ignore the first attempt because of outlier time numbers
+        if line.startswith(searchThreadPhrase) and (line.find('Attempt: 1') < 0 ):
+            print line
+            startFlag = True
+            
+        elif line.startswith(endingMemoryPhrase):
+            startFlag = False
+            
+        elif startFlag and line.startswith('Time'):
+            tokens = line.split()
+#             print tokens
+            
+            time.append(float(tokens[1]))
+            diskReads.append(int(tokens[3]))
+            bufferReads.append(int(tokens[5]))
+            
+#     print time
+#     print diskReads
+#     print bufferReads      
+            
+    return [np.mean(time), np.mean(diskReads), np.mean(bufferReads)]
+
+'''
+parse data for all trials with the given range of thread numbers
+Input:
+filename: the log file
+threadNums: the given range of thread numbers
+Output:
+n x 3 array: average time, disk reads, buffere reads for each thread number
+'''    
+def parseDataRange(filename, threadNums):
+    data = []
+    
+    for idx in range(0,len(threadNums)):
+        num =threadNums[idx]
+        
+        row = parseDataThread(filename, num)
+        data.append(row)
+
+    return data
+
+from sys import argv
+
+# For testing and debugging
+lcFilename = r"..\data\results_firstrun_2000_2000.txt"
+scFilename = r"..\data\results_secondrun_2000_2000.txt"
+r112Filename = r'..\data\results_baseline.txt'
+    
+fileList = []
+pngFilename = ''
+
+if ( len(argv) < 2 ):
+    fileList = [lcFilename, scFilename, r112Filename]
+    pngFilename = "LogPlot.png"
+elif ( len(argv) == 2 ):
+    fileList = [lcFilename, scFilename, r112Filename]
+    pngFilename = argv[1]
+else:
+    pngFilename = argv[1]
+    # print argv[2:]
+    fileList = argv[2:]
+
+threadNumList = range(1,6)
+# 3 subplots for average time, disk reads, and buffer reads
+# Another one for legends
+figure, plots = plt.subplots(3, 1, True )
+# Adjust this number so that the ylabels of the subplots are aligned
+labelx = -0.11
+
+# This fileList will be replaced by command-line arguments
+fileLegend = []
+# Only use the last 45 chars for legend
+for file in fileList:
+    fileLegend.append( file[-45:])
+
+colors = ['bo-', 'g*-', 'rs-', 'c8-', 'm<-', 'kv-']
+
+for idx in range(0,len(fileList)):
+    data = parseDataRange(fileList[idx],threadNumList)
+#     print data
+    
+    matrix = np.array(data)
+    
+    # Plotting
+    timePlot, = plots[0].plot( threadNumList, matrix[:,0], colors[idx] )
+    plots[0].set_ylabel( "Scan time" )
+    
+    diskPlot, = plots[1].plot( threadNumList, matrix[:,1], colors[idx] )
+    plots[1].set_ylabel( "Disk reads" )
+    
+    bufferPlot, = plots[2].plot( threadNumList, matrix[:,2], colors[idx] )
+    plots[2].set_ylabel( "Buffer reads" )
+    
+# Common settings for the subplots
+for idx in range(0,3):
+    plots[idx].yaxis.set_label_coords(labelx, 0.5)
+    plots[idx].set_xticks(range(0,7))
+    plots[idx].grid(True)
+
+# Add legend
+# plots[0].legend(fileLegend, bbox_to_anchor=(0.025, 0.92, 0.95, .102), loc=3, mode="expand", borderaxespad=0.)
+lgd = plots[0].legend(fileLegend, bbox_to_anchor=(0., 1.02, 1., .102), loc=3, mode="expand", borderaxespad=0.)
+
+plt.xlabel('Thread number')
+plt.savefig(pngFilename, dpi = 300, bbox_extra_artists=(lgd,), bbox_inches='tight' )
+plt.show()
