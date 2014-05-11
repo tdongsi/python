d1db42a9f5ecd03d808f6052140441af33030c43
Tue-Cuong Dong-Si <dongsi.tuecuong@gmail.com>
2014-05-10 20:39:42 -0700

    Small programs for Quiz 4 and 6.

---
+++ InteractivePython/src/BlackJack.py
@@ -0,0 +1,182 @@
+'''
+Created on May 10, 2014
+
+@author: tdongsi
+'''
+
+n = 5
+for trial in range(12):
+    n *= 2
+    n -= 3
+    
+print n
+
+# n = 127834876
+# while n >= 0:
+#     n //= 2
+
+#########################################
+# Question 7
+n = 1000
+numbers = range(2,n)
+
+results = []
+
+while ( len(numbers) > 0 ):
+    prime = numbers[0]
+    results.append(numbers[0])
+    numbers = [number for number in numbers if (number % prime != 0) ]
+    
+print "Length: %d" % len(results)
+#########################################
+
+#########################################
+# Question 8
+fast = 1.0
+slow = 1000.0
+year = 1
+
+while fast < slow:
+    year += 1
+    fast *= 1.4
+    slow *= 1.2
+    
+print "Year: %d" % (year-1)
+#########################################
+
+print "%f %f" % (12*73 + 36.5, 3*98 + 49)
+
+class Overload:
+    def __init__(self, value1):
+        pass
+    
+
+class BankAccount:
+    def __init__(self, initial_balance):
+        """Creates an account with the given balance."""
+        self.balance = initial_balance
+        self.fee = 0
+        
+    def deposit(self, amount):
+        """Deposits the amount into the account."""
+        self.balance += amount
+    
+    def withdraw(self, amount):
+        """
+        Withdraws the amount from the account.  Each withdrawal resulting in a
+        negative balance also deducts a penalty fee of 5 dollars from the balance.
+        """
+        self.balance -= amount
+        if ( self.balance < 0):
+            self.balance -= 5
+            self.fee += 5
+    
+    def get_balance(self):
+        """Returns the current balance in the account."""
+        return self.balance
+    
+    def get_fees(self):
+        """Returns the total fees ever deducted from the account."""
+        return self.fee
+    
+my_account = BankAccount(10)
+my_account.withdraw(5)
+my_account.deposit(10)
+my_account.withdraw(5)
+my_account.withdraw(15)
+my_account.deposit(20)
+my_account.withdraw(5) 
+my_account.deposit(10)
+my_account.deposit(20)
+my_account.withdraw(15)
+my_account.deposit(30)
+my_account.withdraw(10)
+my_account.withdraw(15)
+my_account.deposit(10)
+my_account.withdraw(50) 
+my_account.deposit(30)
+my_account.withdraw(15)
+my_account.deposit(10)
+my_account.withdraw(5) 
+my_account.deposit(20)
+my_account.withdraw(15)
+my_account.deposit(10)
+my_account.deposit(30)
+my_account.withdraw(25) 
+my_account.withdraw(5)
+my_account.deposit(10)
+my_account.withdraw(15)
+my_account.deposit(10)
+my_account.withdraw(10) 
+my_account.withdraw(15)
+my_account.deposit(10)
+my_account.deposit(30)
+my_account.withdraw(25) 
+my_account.withdraw(10)
+my_account.deposit(20)
+my_account.deposit(10)
+my_account.withdraw(5) 
+my_account.withdraw(15)
+my_account.deposit(10)
+my_account.withdraw(5) 
+my_account.withdraw(15)
+my_account.deposit(10)
+my_account.withdraw(5) 
+print my_account.get_balance(), my_account.get_fees()
+
+account1 = BankAccount(20)
+account1.deposit(10)
+account2 = BankAccount(10)
+account2.deposit(10)
+account2.withdraw(50)
+account1.withdraw(15)
+account1.withdraw(10)
+account2.deposit(30)
+account2.withdraw(15)
+account1.deposit(5)
+account1.withdraw(10)
+account2.withdraw(10)
+account2.deposit(25)
+account2.withdraw(15)
+account1.deposit(10)
+account1.withdraw(50)
+account2.deposit(25)
+account2.deposit(25)
+account1.deposit(30)
+account2.deposit(10)
+account1.withdraw(15)
+account2.withdraw(10)
+account1.withdraw(10)
+account2.deposit(15)
+account2.deposit(10)
+account2.withdraw(15)
+account1.deposit(15)
+account1.withdraw(20)
+account2.withdraw(10)
+account2.deposit(5)
+account2.withdraw(10)
+account1.deposit(10)
+account1.deposit(20)
+account2.withdraw(10)
+account2.deposit(5)
+account1.withdraw(15)
+account1.withdraw(20)
+account1.deposit(5)
+account2.deposit(10)
+account2.deposit(15)
+account2.deposit(20)
+account1.withdraw(15)
+account2.deposit(10)
+account1.deposit(25)
+account1.deposit(15)
+account1.deposit(10)
+account1.withdraw(10)
+account1.deposit(10)
+account2.deposit(20)
+account2.withdraw(15)
+account1.withdraw(20)
+account1.deposit(5)
+account1.deposit(10)
+account2.withdraw(20)
+print account1.get_balance(), account1.get_fees(), account2.get_balance(), account2.get_fees()
+
