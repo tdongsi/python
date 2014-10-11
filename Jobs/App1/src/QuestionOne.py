f213b81619423ed0b2f93a428df32a627b309dee
Tue-Cuong Dong-Si <dongsi.tuecuong@gmail.com>
2014-10-10 23:40:52 -0700

    Some unit test cases for Question 1.

---
+++ Jobs/App1/src/QuestionOne.py
@@ -0,0 +1,14 @@
+'''
+Created on Oct 10, 2014
+
+@author: tdongsi
+'''
+
+class AnswerOne(object):
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ Jobs/App1/test/QuestionOneTest.py
@@ -0,0 +1,46 @@
+'''
+Created on Sep 24, 2014
+
+@author: tdongsi
+'''
+import unittest
+from QuestionOne import *
+
+class Test(unittest.TestCase):
+
+    def test_fileOne_functional(self):
+        answer = AnswerOne('../test/Q1Test1.txt')
+        self.assertEqual(answer._getValueSet('X1'), set(['X1', 'X2', 'X4', '1', '2', '3','X5', 'X3', 'X6', 'X7']))
+        self.assertEqual(answer._getValueSet('X2'), set(['X2', 'X4', '1', '2', '3','X5']))
+        self.assertEqual(answer._getValueSet('X3'), set(['X3', 'X6', 'X7']))
+        self.assertEqual(answer._getValueSet('X4'), set(['X4', '1', '2', '3']))
+
+
+    def test_fileOne_invalid(self):
+        answer = AnswerOne('../test/Q1Test1.txt')
+        self.assertEqual(answer.getValueString('X5'),'')
+        self.assertEqual(answer.getValueString('X6'),'')
+        self.assertEqual(answer.getValueString('X7'),'')
+        self.assertEqual(answer.getValueString('NonExistent'),'')
+    
+    def test_fileTwo_functional(self):
+        answer = AnswerOne('../test/Q1Test2.txt')
+        self.assertEqual(answer._getValueSet('X1'), set(['X1', 'X2', 'X3', '1', '2', '3','X4', 'X5']))
+        self.assertEqual(answer._getValueSet('X2'), set(['X2', 'X4', '1', '2', '3']))
+        self.assertEqual(answer._getValueSet('X3'), set(['X3', 'X4', '1', '2', '3']))
+        self.assertEqual(answer._getValueSet('X4'), set(['X4', '1', '2', '3']))
+
+
+    def test_fileTwo_invalid(self):
+        answer = AnswerOne('../test/Q1Test2.txt')
+        self.assertEqual(answer.getValueString('X5'),'')
+        self.assertEqual(answer.getValueString('X6'),'')
+        self.assertEqual(answer.getValueString('NonExistent'),'')
+            
+    def test_emptyfile(self):
+        answer =  AnswerOne('../test/Q1Test3.txt')
+        self.assertEqual(answer.getValueString('NonExistent'),'')
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
