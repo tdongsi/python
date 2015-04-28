8ba04481881e4cf5faba31058f06ab5ae25b3982
tdongsi <dongsi.tuecuong@gmail.com>
2015-04-28 16:45:23 -0700

    Clean up. Add commonly used Python methods.

--- Scripts/src/MyLogger.py
+++ Scripts/src/MyLogger.py
@@ -7,9 +7,10 @@ Created on Jul 28, 2014
 import logging
 import subprocess
 import inspect
+from contextlib import contextmanager
+import os
 
 # set up logging to file
-# Extension *.mylog to avoid being deleted by setupEdgeIngest.py
 LOG_FILENAME = 'installCheck.mylog'
 # Additional logging info: %(asctime)s %(name)-12s 
 logging.basicConfig(level=logging.DEBUG,
@@ -29,16 +30,35 @@ console.setFormatter(formatter)
 logging.getLogger('').addHandler(console)
 
 
-def runCommand(cmdStr, logger):
+# create logger
+myLogger = logging.getLogger('IncrementalLoad')
+
+
+def runCommand(cmdStr, logger = myLogger, envMap = None):
     # Print out the caller module and its line number
-    logger.debug( 'Calling from %s' % str(inspect.stack()[1][1:3]))
-    logger.debug(cmdStr)
+    logger.debug( 'Calling from %s', str(inspect.stack()[1][1:3]))
+    logger.info( '> %s', ' '.join(cmdStr))
     try:
-        output = subprocess.check_output( cmdStr, stderr=subprocess.STDOUT )
+        output = subprocess.check_output( cmdStr, stderr=subprocess.STDOUT,
+                  env = envMap )
         logger.debug(output)
     except subprocess.CalledProcessError as e:
-        logger.error( "Error code: %d" % e.returncode)
+        logger.error( "Error code: %d", e.returncode)
         logger.error(e.output)
+    return
+
+@contextmanager
+def working_directory(path):
+    '''
+    Switch to a new working directory and back to the current, using with statement.
+    '''
+    
+    current_dir = os.getcwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(current_dir)
         
         
 if __name__ == "__main__":
