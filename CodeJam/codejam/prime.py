e124836b743e9f1748a69e069ce14d97397449be
tdongsi <dongsi.tuecuong@gmail.com>
2016-04-09 16:45:08 -0700

    Modify Rabin Miller trials to find factors.

--- CodeJam/codejam/prime.py
+++ CodeJam/codejam/prime.py
@@ -1,4 +1,5 @@
 
+import fractions
 import random
 
 
@@ -53,6 +54,8 @@ def is_pseudo_prime(prime, trial=10):
     if prime < 2:
         return False
     for p in SMALL_PRIMES:
+        if p * p > prime:
+            return True
         if prime % p == 0:
             return False
 
@@ -67,7 +70,7 @@ def is_pseudo_prime(prime, trial=10):
     return True
 
 
-def find_factor(prime, trial=10):
+def find_factor(prime, trial=100):
     """ Modify Rabin Miller test of primality to find factor of composite.
 
     :param prime: the input number.
@@ -76,44 +79,67 @@ def find_factor(prime, trial=10):
     """
 
     # small primes < 100
-    SMALL_PRIMES = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
-                    43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+    SMALL_PRIMES = [ 2,   3,   5,   7,  11,  13,  17,  19,  23,  29,  31,  37,  41,
+                  43,  47,  53,  59,  61,  67,  71,  73,  79,  83,  89,  97, 101,
+                 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167,
+                 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239,
+                 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313,
+                 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397,
+                 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467,
+                 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569,
+                 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643,
+                 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733,
+                 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823,
+                 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911,
+                 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997]
 
     def rabin_miller_trial(prime, num):
-        """ Check if prime pass the Rabin-Miller trial.
+        """ Find factor based on the Rabin-Miller trial.
 
         :param prime: a prospective prime.
         :param num: a random "witness" of primality.
 
-        :return: True if composite, False if probably prime.
+        :return: > 1 if composite, 1 if probably prime.
         """
         num = pow(num, remainder, prime)
 
         # For first iteration, 1 or -1 remainder implies prime
         if num == 1 or num == prime - 1:
-            return False
+            return 1
+        else:
+            gcd = fractions.gcd(num-1, prime)
+            if gcd > 1:
+                return gcd
 
         # For next iterations, -1 implies prime, 1 implies composite
         for _ in xrange(exponent):
             num = pow(num, 2, prime)
             if num == prime - 1:
-                return False
+                return 1
+            else:
+                gcd = fractions.gcd(num-1, prime)
+                if gcd > 1:
+                    return gcd
 
-        return True
+        # It is a composite, but could not find a factor
+        return -1
 
     # Labor saving steps
     if prime < 2:
-        return False
+        raise ValueError("Unexpected input")
     for p in SMALL_PRIMES:
+        if p * p > prime:
+            return 1
         if prime % p == 0:
-            return False
+            return p
 
     # Starting Rabin-Miller algorithm
     exponent, remainder = decompose(prime - 1)
 
     for _ in xrange(trial):
         num = random.randint(2, prime - 2)
-        if rabin_miller_trial(prime, num):
-            return False
+        factor = rabin_miller_trial(prime, num)
+        if factor > 1:
+            return factor
 
-    return True
+    return 1
--- CodeJam/tests/test_prime.py
+++ CodeJam/tests/test_prime.py
@@ -11,3 +11,18 @@ class PseudoPrimeTest(unittest.TestCase):
         self.assertFalse(pr.is_pseudo_prime(703))
         self.assertTrue(pr.is_pseudo_prime(701))
         self.assertTrue(pr.is_pseudo_prime(977))
+
+        self.assertFalse(pr.is_pseudo_prime(443*991))
+        self.assertFalse(pr.is_pseudo_prime(601*811))
+
+    def test_find_factor(self):
+
+        self.assertTrue(pr.find_factor(561) > 1)
+        self.assertTrue(pr.find_factor(703) > 1)
+        self.assertEqual(pr.find_factor(701), 1)
+        self.assertEqual(pr.find_factor(977), 1)
+
+        self.assertTrue(pr.find_factor(341) > 1)
+
+        self.assertTrue(pr.find_factor(443*991) > 1)
+        # self.assertEqual(pr.find_factor(601*811, 50), 601)
