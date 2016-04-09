956da700b0db64a10d1350bc05c8feaf28fe4189
tdongsi <dongsi.tuecuong@gmail.com>
2016-04-09 16:03:19 -0700

    Code Jam 2016: Add labor saving steps. Add a copy before modifying.

--- CodeJam/codejam/prime.py
+++ CodeJam/codejam/prime.py
@@ -23,6 +23,10 @@ def is_pseudo_prime(prime, trial=10):
     :return: True if all trials passed, False if not.
     """
 
+    # small primes < 100
+    SMALL_PRIMES = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
+                    43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+
     def rabin_miller_trial(prime, num):
         """ Check if prime pass the Rabin-Miller trial.
 
@@ -45,6 +49,66 @@ def is_pseudo_prime(prime, trial=10):
 
         return True
 
+    # Labor saving steps
+    if prime < 2:
+        return False
+    for p in SMALL_PRIMES:
+        if prime % p == 0:
+            return False
+
+    # Starting Rabin-Miller algorithm
+    exponent, remainder = decompose(prime - 1)
+
+    for _ in xrange(trial):
+        num = random.randint(2, prime - 2)
+        if rabin_miller_trial(prime, num):
+            return False
+
+    return True
+
+
+def find_factor(prime, trial=10):
+    """ Modify Rabin Miller test of primality to find factor of composite.
+
+    :param prime: the input number.
+    :param trial: Number of Rabin-Miller trials.
+    :return: 1 if prime (all trials passed), > 1 if composite.
+    """
+
+    # small primes < 100
+    SMALL_PRIMES = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
+                    43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+
+    def rabin_miller_trial(prime, num):
+        """ Check if prime pass the Rabin-Miller trial.
+
+        :param prime: a prospective prime.
+        :param num: a random "witness" of primality.
+
+        :return: True if composite, False if probably prime.
+        """
+        num = pow(num, remainder, prime)
+
+        # For first iteration, 1 or -1 remainder implies prime
+        if num == 1 or num == prime - 1:
+            return False
+
+        # For next iterations, -1 implies prime, 1 implies composite
+        for _ in xrange(exponent):
+            num = pow(num, 2, prime)
+            if num == prime - 1:
+                return False
+
+        return True
+
+    # Labor saving steps
+    if prime < 2:
+        return False
+    for p in SMALL_PRIMES:
+        if prime % p == 0:
+            return False
+
+    # Starting Rabin-Miller algorithm
     exponent, remainder = decompose(prime - 1)
 
     for _ in xrange(trial):
