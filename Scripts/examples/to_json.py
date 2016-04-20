f9c071e8f2352bf3d5a15678231139d22c8acc70
tdongsi <dongsi.tuecuong@gmail.com>
2016-04-20 14:52:34 -0700

    JSON serialization: Use OrderedDict as base class.

--- Scripts/examples/to_json.py
+++ Scripts/examples/to_json.py
@@ -1,9 +1,13 @@
-
+import collections
 import json
 
 
 def generate_count_queries():
     return "TODO"
+
+
+class OrderedConfig(collections.OrderedDict):
+    pass
 
 
 class Config(object):
@@ -57,11 +61,31 @@ def create_config_file(filename, query_generator):
         json.dump(config, config_file, default=lambda o: o.__dict__, indent=4)
 
 
+def ordered_config_file(filename, query_generator):
+
+    hive_source = OrderedConfig()
+    hive_source["type"] = "hive"
+    hive_source["url"] = "jdbc:hive2://192.168.5.184:10000/DWH"
+    vertica_target = OrderedConfig()
+    vertica_target["type"] = "vertica"
+    vertica_target["url"] = "jdbc:vertica://192.168.5.174:5433/VMart"
+
+    config = OrderedConfig()
+    config["source"] = hive_source
+    config["target"] = vertica_target
+    config["testName"] = "count"
+    config["queries"] = query_generator
+
+    with open(filename, 'w') as config_file:
+        json.dump(config, config_file, indent=4)
+
+
 def main():
 
     FILE_NAME = "hive_vertica_count.json"
     query_generator = generate_count_queries()
     create_config_file(FILE_NAME, query_generator)
+    ordered_config_file(FILE_NAME, query_generator)
 
 
 if __name__ == "__main__":
