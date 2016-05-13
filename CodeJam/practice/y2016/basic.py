f5443800b9aca4d26093ddcb7198de70f792f235
tdongsi <dongsi.tuecuong@gmail.com>
2016-05-13 13:25:02 -0700

    Practice: Another way of binary search.

--- CodeJam/practice/y2016/basic.py
+++ CodeJam/practice/y2016/basic.py
@@ -1,5 +1,37 @@
 
-def bin_search(mlist, num):
+def bin_search(mlist, num, start=0, end=None):
+    """ Binary search.
+
+    :param mlist:
+    :param num:
+    :return: -1 if num is not found in the list
+    """
+    if start < 0:
+        raise ValueError("start cannot be negative")
+    if end is None:
+        end = len(mlist)
+
+    if start == end:
+        # empty sublist
+        return -1
+
+    if start == end-1:
+        # singleton sublist
+        if mlist[start] == num:
+            return start
+        else:
+            return -1
+
+    med = (start + end) // 2
+    if mlist[med] == num:
+        return med
+    elif mlist[med] > num:
+        return bin_search(mlist, num, start, med)
+    else:
+        return bin_search(mlist, num, med+1, end)
+
+
+def bin_search_old(mlist, num):
     """ Binary search.
 
     :param mlist:
