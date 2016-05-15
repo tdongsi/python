7b20f7fe17ca2dab1c94523f1d23279d33ed0263
tdongsi <dongsi.tuecuong@gmail.com>
2016-05-15 01:50:43 -0700

    Practice: Better test class for practice.

--- CodeJam/tests/y2016/test_basic.py
+++ CodeJam/tests/y2016/test_basic.py
@@ -3,7 +3,8 @@ import unittest
 import random
 
 from practice.y2016.basic import heap_sort as do_sort
-import practice.y2016.basic as bs
+from practice.y2016.basic import bin_search as binary_search
+from practice.y2016.basic import atoi as atoi
 
 
 class TestSorting(unittest.TestCase):
@@ -22,26 +23,24 @@ class TestSorting(unittest.TestCase):
         pass
 
 
-
-
 class TestBasic(unittest.TestCase):
 
     def test_binary_search(self):
-        self.assertEqual(bs.bin_search([1, 2, 3], 4), -1)
-        self.assertEqual(bs.bin_search([1, 2, 3], 3), 2)
-        self.assertEqual(bs.bin_search([1, 2, 3], 2), 1)
-        self.assertEqual(bs.bin_search([1, 2, 3], 1), 0)
+        self.assertEqual(binary_search([1, 2, 3], 4), -1)
+        self.assertEqual(binary_search([1, 2, 3], 3), 2)
+        self.assertEqual(binary_search([1, 2, 3], 2), 1)
+        self.assertEqual(binary_search([1, 2, 3], 1), 0)
 
-        self.assertEqual(bs.bin_search([], 2), -1)
-        self.assertEqual(bs.bin_search([1], 2), -1)
-        self.assertEqual(bs.bin_search([1], -2), -1)
+        self.assertEqual(binary_search([], 2), -1)
+        self.assertEqual(binary_search([1], 2), -1)
+        self.assertEqual(binary_search([1], -2), -1)
 
-        self.assertEqual(bs.bin_search([1, 2, 3, 4, 5], 3), 2)
-        self.assertEqual(bs.bin_search([1, 2, 3, 4, 5], 2), 1)
-        self.assertEqual(bs.bin_search([1, 2, 3, 4, 5], 1), 0)
+        self.assertEqual(binary_search([1, 2, 3, 4, 5], 3), 2)
+        self.assertEqual(binary_search([1, 2, 3, 4, 5], 2), 1)
+        self.assertEqual(binary_search([1, 2, 3, 4, 5], 1), 0)
 
     def test_atoi(self):
-        self.assertEqual(bs.atoi("123"), int("123"))
-        self.assertEqual(bs.atoi("-123"), int("-123"))
-        self.assertEqual(bs.atoi("0"), int("0"))
-        self.assertEqual(bs.atoi("-0"), int("-0"))
+        self.assertEqual(atoi("123"), int("123"))
+        self.assertEqual(atoi("-123"), int("-123"))
+        self.assertEqual(atoi("0"), int("0"))
+        self.assertEqual(atoi("-0"), int("-0"))
